ad209a31f3c6f901eca9698bbdc56a4b81a59e51
palamccc <palam.c@gmail.com>
2016-07-19 05:28:51 +0530

    fixed typo

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 
-const rand = Math.random().toString().subtr(2, 8);
+const rand = Math.random().toString().substr(2, 8);
 require('http').createServer((req, res) => res.end(`hello world ${rand} ${Date.now()}`)).listen(3000);
