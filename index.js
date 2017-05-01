21429193ea0939cc1c3b69d1f7108bf0e23856df
Kinjal Rathod <Kinjal_Rathod@intuit.com>
2017-05-01 10:40:20 -0700

    New change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 const rand = Math.random().toString().substr(2, 8);
-require('http').createServer((req, res) => res.end(`hello world ${rand} ${Date.now()}`)).listen(3000);
+require('http').createServer((req, res) => res.end(`hello new world ${rand} ${Date.now()}`)).listen(3000);
 
 
